a7fd662d96299ff18d19d8e62d1d5882f8a22f53
Igor Oliveira <igoroliveira.oliveira@gmail.com>
2014-10-08 13:57:57 -0300

    Window

---
+++ main.rb
@@ -0,0 +1,17 @@
+require "gosu"
+
+class Window < Gosu::Window
+	def initialize width = 800, height = 600, fullscreen = false
+		super
+	end
+
+	def update
+		
+	end
+
+	def draw
+		
+	end
+end
+
+window = Window.new.show
