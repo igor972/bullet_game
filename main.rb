b6621b82cea3df70fd2cff69eed1367192b0623e
Igor Oliveira <igoroliveira.oliveira@gmail.com>
2014-10-08 16:07:24 -0300

    launch bullet

--- main.rb
+++ main.rb
@@ -7,6 +7,12 @@ class Game
 		@letter = Gosu::Image.from_text @window, "E", Gosu.default_font_name, 100
 		@letter_x = 0
 		@letter_y = @window.height/2 - @letter.height/2
+
+		#Bullet
+		@bullet = Gosu::Image.from_text @window, ">", Gosu.default_font_name, 50
+		@bullet_x = 0
+		@bullet_y = @letter_y
+		@bullet_launched = false
 	end
 
 	def update
@@ -17,12 +23,40 @@ class Game
 		if @window.button_down? Gosu::KbDown and @letter_y <= 500
 			@letter_y += 8
 		end
+
+		if @window.button_down? Gosu::KbSpace 
+			@bullet_launched = true
+			if @bullet_x == 0
+				@bullet_y = @letter_y
+			end
+		end
+
+		if @bullet_x <= 500 and @bullet_launched == true
+			@bullet_x +=10
+		else
+			@bullet_x = 0
+			@bullet_launched = false
+		end
 	end
 
 	def draw
 		@letter.draw @letter_x, @letter_y, 0
+		
+		if @bullet_launched == true
+			@bullet.draw @bullet_x, @bullet_y, 0
+		end
 	end
 end
+
+
+
+
+
+
+
+
+
+
 
 class Window < Gosu::Window
 	def initialize width = 800, height = 600, fullscreen = false
