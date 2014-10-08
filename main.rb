c1bef624b5bff8ec2d821b71f7b5bc851df6bf37
Igor Oliveira <igoroliveira.oliveira@gmail.com>
2014-10-08 15:21:05 -0300

    add moviment

--- main.rb
+++ main.rb
@@ -1,16 +1,41 @@
 require "gosu"
 
-class Window < Gosu::Window
-	def initialize width = 800, height = 600, fullscreen = false
-		super
+class Game
+	def initialize window
+		@window = window
+		
+		@letter = Gosu::Image.from_text @window, "E", Gosu.default_font_name, 100
+		@letter_x = 0
+		@letter_y = @window.height/2 - @letter.height/2
 	end
 
 	def update
-		
+		if @window.button_down? Gosu::KbUp
+			@letter_y -= 8	
+		end
+
+		if @window.button_down? Gosu::KbDown
+			@letter_y += 8
+		end
 	end
 
 	def draw
-		
+		@letter.draw @letter_x, @letter_y, 0
+	end
+end
+
+class Window < Gosu::Window
+	def initialize width = 800, height = 600, fullscreen = false
+		super
+		@game = Game.new self
+	end
+
+	def update
+		@game.update
+	end
+
+	def draw
+		@game.draw
 	end
 end
 
