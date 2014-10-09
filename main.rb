f624823ced333689910ba4741584fbd143e92e1a
Igor Oliveira <igoroliveira.oliveira@gmail.com>
2014-10-09 17:23:45 -0300

    class Player

--- main.rb
+++ main.rb
@@ -1,57 +1,45 @@
 require "gosu"
 
 class Game
+	attr_accessor :window
+
 	def initialize window
 		@window = window
-		
-		@letter = Gosu::Image.from_text @window, "E", Gosu.default_font_name, 100
-		@letter_x = 0
-		@letter_y = @window.height/2 - @letter.height/2
-
-		#Bullet
-		@bullet = Gosu::Image.from_text @window, ">", Gosu.default_font_name, 50
-		@bullet_x = 0
-		@bullet_y = @letter_y
-		@bullet_launched = false
+		@player = Player.new self
 	end
 
 	def update
-		if @window.button_down? Gosu::KbUp and @letter_y >= 0
-			@letter_y -= 8	
-		end
-
-		if @window.button_down? Gosu::KbDown and @letter_y <= 500
-			@letter_y += 8
-		end
-
-		if @window.button_down? Gosu::KbSpace 
-			@bullet_launched = true
-			if @bullet_x == 0
-				@bullet_y = @letter_y
-			end
-		end
-
-		if @bullet_x <= 500 and @bullet_launched == true
-			@bullet_x +=10
-		else
-			@bullet_x = 0
-			@bullet_launched = false
-		end
+		@player.update
 	end
 
 	def draw
-		@letter.draw @letter_x, @letter_y, 0
-		
-		if @bullet_launched == true
-			@bullet.draw @bullet_x, @bullet_y, 0
-		end
+		@player.draw
 	end
 end
 
 
 
 
+class Player
+	attr_accessor :x, :y, :game
 
+	def initialize game
+		@game = game
+		@image = Gosu::Image.from_text @game.window, "E", Gosu.default_font_name, 100
+
+		@x = 0
+		@y = @game.window.height/2 - @image.height/2
+	end
+
+	def update
+		@y += 10 if @game.window.button_down? Gosu::KbDown and @y <= 500
+		@y -= 10 if @game.window.button_down? Gosu::KbUp and @y >= 0
+	end
+
+	def draw
+		@image.draw @x, @y, 0
+	end
+end
 
 
 
