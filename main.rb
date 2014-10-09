54dcb46070859d64ef7cc72ecdd48d63b33f1e2a
Igor Oliveira <igoroliveira.oliveira@gmail.com>
2014-10-09 18:01:17 -0300

    bullet class

--- main.rb
+++ main.rb
@@ -6,19 +6,25 @@ class Game
 	def initialize window
 		@window = window
 		@player = Player.new self
+		@bullets = []
 	end
 
 	def update
 		@player.update
+
+		if @window.button_down?(Gosu::KbSpace)
+			@bullets.push(Bullet.new(self, 0, @player.y))
+		end
+
+		# update bullet and remove from array 'dead bullets'
+		@bullets.each {|bullet| if bullet.live == true then bullet.update else @bullets.delete(bullet) end}
 	end
 
 	def draw
 		@player.draw
+		@bullets.each {|bullet| bullet.draw}
 	end
 end
-
-
-
 
 class Player
 	attr_accessor :x, :y, :game
@@ -41,10 +47,29 @@ class Player
 	end
 end
 
+class Bullet
+	attr_accessor :x, :y, :game, :live
 
+	def initialize game, x, y
+		@game = game
+		@image = Gosu::Image.from_text @game.window, ">", Gosu.default_font_name, 50
+		@x = x
+		@y = y
+		@live = true
+	end
 
+	def update
+		if @x <= (@game.window.width - 20) and @live == true
+			@x += 10 
+		else
+			@live = false
+		end
+	end
 
-
+	def draw
+		@image.draw @x, @y, 0 unless @live == false
+	end
+end
 
 class Window < Gosu::Window
 	def initialize width = 800, height = 600, fullscreen = false
