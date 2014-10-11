0935996c952f673dd3adf095f8f654ec73481406
Igor Oliveira <igoroliveira.oliveira@gmail.com>
2014-10-11 01:46:46 -0300

    Some changes on Size: Window, Player and Bullet

--- main.rb
+++ main.rb
@@ -31,14 +31,14 @@ class Player
 
 	def initialize game
 		@game = game
-		@image = Gosu::Image.from_text @game.window, "E", Gosu.default_font_name, 100
+		@image = Gosu::Image.from_text @game.window, "E", Gosu.default_font_name, 50
 
 		@x = 0
 		@y = @game.window.height/2 - @image.height/2
 	end
 
 	def update
-		@y += 10 if @game.window.button_down? Gosu::KbDown and @y <= 500
+		@y += 10 if @game.window.button_down? Gosu::KbDown and @y <= @game.window.height - @image.height
 		@y -= 10 if @game.window.button_down? Gosu::KbUp and @y >= 0
 	end
 
@@ -52,7 +52,7 @@ class Bullet
 
 	def initialize game, x, y
 		@game = game
-		@image = Gosu::Image.from_text @game.window, ">", Gosu.default_font_name, 50
+		@image = Gosu::Image.from_text @game.window, ">", Gosu.default_font_name, 30
 		@x = x
 		@y = y
 		@live = true
@@ -72,7 +72,7 @@ class Bullet
 end
 
 class Window < Gosu::Window
-	def initialize width = 800, height = 600, fullscreen = false
+	def initialize width = 1200, height = 600, fullscreen = false
 		super
 		@game = Game.new self
 	end
